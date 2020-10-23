91cb9d6c3ea5bc559c226c105744e92f023edd48
Crowdbotics <team@crowdbotics.com>
2020-10-23 11:50:18 +0000

    add package blueprint-react-native-maps-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Maps157181', human_name: 'Maps', access_route: 'Maps157181', icon: 'map'},
 { name: 'UserProfile157177', human_name: 'User Profile', access_route: 'UserProfile157177'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Maps157181Navigator from '../features/Maps157181/navigator';
 import UserProfile157177Navigator from '../features/UserProfile157177/navigator';
 
 /**
@@ -14,6 +15,7 @@ import UserProfile157177Navigator from '../features/UserProfile157177/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Maps157181: { screen: Maps157181Navigator },
 UserProfile157177: { screen: UserProfile157177Navigator },
 
     /** new navigators can be added here */
